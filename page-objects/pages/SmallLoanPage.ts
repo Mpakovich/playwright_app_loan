3853fb9bd34d3eba7ea3668e0b1c64334430e176
Mpakovich <56260995f@gmail.com>
2025-03-12 16:33:33 +0200

    Homework17

--- page-objects/pages/SmallLoanPage.ts
+++ page-objects/pages/SmallLoanPage.ts
@@ -1,43 +1,62 @@
-import {Locator, Page} from "@playwright/test";
-import { Button } from "../atoms/Button";
-import { Input } from "../atoms/input";
+import { expect, Locator, Page } from '@playwright/test';
+import { Button } from '../atoms/Button';
+import { Input } from '../atoms/input';
 
-const url = "https://loan-app.tallinn-learning.ee/small-loan"
+const url = 'https://loan-app.tallinn-learning.ee/small-loan';
 
 export class SmallLoanPage {
-    readonly page: Page
-    readonly applyButton: Button
-    readonly applyImage1: Button
-    readonly applyImage2: Button
-    readonly amountInput: Input
-    readonly periodSelect: Locator
-    readonly periodOptions: Locator
-    readonly usernameInput: Input
-    readonly passwordInput: Input
-    readonly continueButton: Button
+  readonly page: Page;
+  readonly applyButton: Button;
+  readonly applyImage1: Button;
+  readonly applyImage2: Button;
+  readonly amountInput: Input;
+  readonly periodSelect: Locator;
+  readonly periodOptions: Locator;
+  readonly usernameInput: Input;
+  readonly passwordInput: Input;
+  readonly continueButton: Button;
+  readonly monthlyAmountSpan: Locator;
+  readonly errorMessage: Locator;
 
+  constructor(page: Page) {
+    this.page = page;
+    this.applyButton = new Button(page, 'id-small-loan-calculator-field-apply');
+    this.applyImage1 = new Button(page, 'id-image-element-button-image-1');
+    this.applyImage2 = new Button(page, 'id-image-element-button-image-2');
+    this.amountInput = new Input(page, 'id-small-loan-calculator-field-amount');
+    this.periodSelect = page.getByTestId('ib-small-loan-calculator-field-period');
+    this.periodOptions = this.periodSelect.locator('option');
+    this.usernameInput = new Input(page, 'login-popup-username-input');
+    this.passwordInput = new Input(page, 'login-popup-password-input');
+    this.continueButton = new Button(page, 'login-popup-continue-button');
+    this.monthlyAmountSpan = page.getByTestId('ib-small-loan-calculator-field-monthlyPayment');
+    this.errorMessage = page.getByTestId('id-small-loan-calculator-field-error');
+  }
 
-    constructor(page: Page) {
-        this.page = page;
-        this.applyButton = new Button(page, "id-small-loan-calculator-field-apply")
-        this.applyImage1 = new Button(page, "id-image-element-button-image-1")
-        this.applyImage2 = new Button(page, "id-image-element-button-image-2")
-        this.amountInput = new Input(page, "id-small-loan-calculator-field-amount")
-        this.periodSelect = page.getByTestId("ib-small-loan-calculator-field-period")
-        this.periodOptions = this.periodSelect.locator("option")
-        this.usernameInput = new Input(page, "login-popup-username-input")
-        this.passwordInput = new Input(page, "login-popup-password-input")
-        this.continueButton = new Button(page,"login-popup-continue-button")
-    }
+  async open(): Promise<void> {
+    await this.page.goto(url);
+  }
 
-    async open(): Promise<void> {
-        await this.page.goto(url)
-    }
+  async getFirstPeriodOption(): Promise<string> {
+    const allOptions = await this.periodOptions.all();
 
-    async getFirstPeriodOption(): Promise<string> {
-        const allOptions = await this.periodOptions.all()
+    return await allOptions[0].innerText();
+  }
 
-        return await allOptions[0].innerText()
+  async checkMonthlyAmount(expected: number): Promise<void> {
+    const innerText = await this.monthlyAmountSpan.innerText();
+    const summ = +innerText.split(' ')[0];
+    expect(expected).toEqual(summ);
+  }
 
-    }
+  async checkErrorDisplayed(): Promise<void> {
+    await expect(this.errorMessage).toBeVisible();
+    await expect(this.errorMessage).toHaveText('Oops, something went wrong');
+  }
+
+  async checkErrorMessageDisplayed(expectedMessage: string): Promise<void> {
+    const innerText = await this.monthlyAmountSpan.innerText();
+    const messageText = innerText.split(' ')[0];
+    expect(messageText).toEqual(expectedMessage);
+  }
 }
---
+++ tests/mock.spec.ts
@@ -0,0 +1,75 @@
+import { test, expect } from '@playwright/test';
+import { SmallLoanPage } from '../page-objects/pages/SmallLoanPage';
+
+test.describe('Loan app mock tests', async () => {
+  test('TL-21-1 positive test', async ({ page }) => {
+    const expectedMonthlyAmount = 100005;
+    const smallLoanPage = new SmallLoanPage(page);
+
+    await page.route('**/api/loan-calc*', async (request) => {
+      const responseBody = { paymentAmountMonthly: expectedMonthlyAmount };
+      await request.fulfill({
+        status: 200,
+        contentType: 'application/json',
+        body: JSON.stringify(responseBody),
+      });
+    });
+
+    const loanCalcResponse = page.waitForResponse('**/api/loan-calc*');
+    await smallLoanPage.open();
+    await loanCalcResponse;
+    await smallLoanPage.checkMonthlyAmount(expectedMonthlyAmount);
+  });
+
+  test('HW-17-1 server error 500 with no response body', async ({ page }) => {
+    const smallLoanPage = new SmallLoanPage(page);
+
+    await page.route('**/api/loan-calc*', async (request) => {
+      await request.fulfill({
+        status: 500,
+        contentType: 'application/json',
+        body: '',
+      });
+    });
+
+    const loanCalcResponse = page.waitForResponse('**/api/loan-calc*');
+    await smallLoanPage.open();
+    await loanCalcResponse;
+    await smallLoanPage.checkErrorDisplayed();
+  });
+
+  test('HW-17-2 200 OK but no response body', async ({ page }) => {
+    const smallLoanPage = new SmallLoanPage(page);
+
+    await page.route('**/api/loan-calc*', async (request) => {
+      await request.fulfill({
+        status: 200,
+        contentType: 'application/json',
+        body: '{}',
+      });
+    });
+
+    const loanCalcResponse = page.waitForResponse('**/api/loan-calc*');
+    await smallLoanPage.open();
+    await loanCalcResponse;
+    await smallLoanPage.checkErrorMessageDisplayed;
+  });
+
+  test('HW-17-3 200 OK but incorrect key in response', async ({ page }) => {
+    const smallLoanPage = new SmallLoanPage(page);
+
+    await page.route('**/api/loan-calc*', async (request) => {
+      const responseBody = { wrongKey: 100005 };
+      await request.fulfill({
+        status: 200,
+        contentType: 'application/json',
+        body: JSON.stringify(responseBody),
+      });
+    });
+
+    const loanCalcResponse = page.waitForResponse('**/api/loan-calc*');
+    await smallLoanPage.open();
+    await loanCalcResponse;
+    await smallLoanPage.checkErrorMessageDisplayed;
+  });
+});
